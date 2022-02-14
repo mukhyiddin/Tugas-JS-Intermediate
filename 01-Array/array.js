50b0003f305b4da2a4063f3ab710ce1728f3b2f9
mukhyiddin <yudivandiego@gmail.com>
2022-02-14 16:18:28 +0800

    Soal 1
    Done Soal 1

---
+++ 01-Array/array.js
@@ -0,0 +1,18 @@
+// Soal 1
+const arr = [1, 2, 3];
+const arr3 = [1, 2, 3];
+const arr2 = [8, 1, 5, 7];
+
+function reverse(number){
+    const array = number.slice().reverse();
+    return array
+}
+
+const newArr = reverse(arr);
+const newArr2 = reverse(arr2);
+
+console.log(" ------------SOAL 1 ----------- ");
+console.log(arr, newArr);
+console.log(arr2, newArr2);
+
+// Soal 2
