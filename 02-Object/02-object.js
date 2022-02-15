a436c6dba9c9be46f552bfc5b23ffd5d0befbdcb
mukhyiddin <yudivandiego@gmail.com>
2022-02-15 14:17:49 +0800

    Objek soal 2
    Mengerjakan soal 2

--- 02-Object/02-object.js
+++ 02-Object/02-object.js
@@ -21,3 +21,32 @@ const person = {
   console.log(person.age);
   console.log(person.favDrinks);
   person.greeting(person.name);
+
+
+  function getObjectValue() {
+      
+  }
+
+const milkBasedCoffee = {
+  name: "latte",
+  ingredients: {
+    espresso: {
+      origin: "lampung",
+      roastProfile: "medium to dark",
+      ratio: 1
+    },
+    milk: {
+      brand: "susu murni",
+      isVegan: false,
+      ratio: 5
+    }
+  },
+}
+
+const espresso = getObjectValue(milkBasedCoffee, "ingredients.espresso.origin");
+const coffeeBrand = getObjectValue(milkBasedCoffee, "ingredients.espresso.brand");
+const isMilkVegan = getObjectValue(milkBasedCoffee, "ingredients.milk.isVegan");
+
+console.log(espresso);
+console.log(coffeeBrand);
+console.log(isMilkVegan)
